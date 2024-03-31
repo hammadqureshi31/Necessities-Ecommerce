ece3501f75003348c8ddd4ab24dd8d25f9c0e83c
hammadqureshi31 <hammad2004qureshi@gmail.com>
2024-03-31 09:09:13 +0500

    fixing bugs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Necessities-Ecommerce/"
+  base: "/Necessities-Ecommerce/",
+  build: {
+    chunkSizeWarningLimit: 1000 * 1024, // 1000 kB (1 MB)
+  },
 })
